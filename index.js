f2efdafd31061fab3f2ba344fa9ae3a82a7d04c7
Bartek Szabat <bartek.szabat@domodi.pl>
2019-10-25 12:48:00 +0200

    1) local time in filenames, 2) parametrized filename prefix

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const operation = require("./lib/operation");
 
 var myArgs = process.argv.slice(2);
 
-if (myArgs.length!==1) {
-    console.error("please pass dir as argument");
+if (myArgs.length<1) {
+    console.error("arguments: directory fileNamePrefix");
     process.exit(-1);
 }
 
-const dir = myArgs[0];
+const [dir,fileNamePrefix] = myArgs;
 
 fs.access(dir, fs.constants.W_OK, function(err) {
     if(err){
         console.error(`dir '${dir}' seems not writable`,err);
         process.exit(1);
     } else {
-        operation.pipeStreamToDir(process.stdin, dir)
+        operation.pipeStreamToDir(process.stdin, dir, fileNamePrefix)
         //     .catch(e=>{
         //     console.error(e);
         //     process.exit(-5);
